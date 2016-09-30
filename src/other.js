37c0d31572d1af74434e3642bfa0939a9d7b447a
Andre Quispesaravia <andreq@fb.com>
2016-09-30 01:22:36 -0700

    basic flow config
    set up flow, we need to fix the three.js require error

---
+++ src/other.js
@@ -0,0 +1,12 @@
+// @flow
+
+const test = require('./test.js');
+const three = require('three');
+
+var x: number = 1;
+var y: number = x + '';
+
+var juanchi = 'la vale';
+console.log(1);
+
+module.exports = juanchi;
---
+++ src/test.js
@@ -0,0 +1,3 @@
+const a: Object = {};
+
+module.exports = a;
