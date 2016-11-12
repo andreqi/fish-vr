94c37e2cf556540fb2cca879a017074d50939bbe
Andre Quispesaravia <andreq@fb.com>
2016-11-12 03:43:35 -0800

    remove unused code

--- src/scene/genRenderLoop.js
+++ src/scene/genRenderLoop.js
@@ -7,9 +7,6 @@ function genRenderLoop(
   update: (t: number) => void,
 ): () => void {
   let t = 0;
-  // controls.autoRotate = true;
-  // controls.autoRotateSpeed = -1
-
   return function renderLoop() {
     controls.update();
     update(t);
